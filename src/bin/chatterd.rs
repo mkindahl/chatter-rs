1132e9e683db89cb44472c6bd89774b37e819274
Mats Kindahl <mats.kindahl@gmail.com>
2019-07-04 19:55:32 +0200

    Command-line option parsing added.
    Using `clap` to handle command-line option parsing.

--- src/bin/chatterd.rs
+++ src/bin/chatterd.rs
@@ -20,57 +20,51 @@ extern crate bytes;
 extern crate chatter;
 extern crate futures;
 
-use chatter::devices::Devices;
-use chatter::gossip::{Gossip, GossipCodec, Message};
-use chatter::view::ServerView;
-use std::env::args;
+use chatter::gossip::{GossipCodec, Message};
+use chatter::state::State;
 use std::net::SocketAddr;
 use std::result::Result;
-use std::sync::{Arc, Mutex};
 use tokio::net::{UdpFramed, UdpSocket};
 use tokio::prelude::*;
+
+use clap::{App, Arg};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
+    let options = App::new("Chatter Agent")
+        .version("0.1")
+        .author("Mats Kindahl <mats.kindahl@gmail.com>")
+        .about("Monitoring agent for distributed systems.")
+        .arg(
+            Arg::with_name("listen")
+                .short("l")
+                .long("listen")
+                .value_name("ADDRESS")
+                .help("Address to listen for gossip on")
+                .takes_value(true),
+        )
+        .get_matches();
+
     let socket = {
-        let sockaddr = args()
-            .nth(1)
-            .unwrap_or("127.0.0.1:8080".to_string())
+        let sockaddr = options
+            .value_of("listen")
+            .unwrap_or("0.0.0.0:2428")
             .parse::<SocketAddr>()?;
         UdpSocket::bind(&sockaddr)?
     };
 
     info!("Listening on {}", socket.local_addr()?);
 
-    // Shared view of all existing agents in the cluster.
-    let shared_view = Arc::new(Mutex::new(ServerView::new()));
-
-    // Shared device information.
-    let shared_devices = Arc::new(Mutex::new(Devices::new()));
+    let shared_state = State::new();
 
     let (mut writer, reader) = UdpFramed::new(socket, GossipCodec::new()).split();
 
     // Future for updating state based on received gossip.
     let update_future = {
-        let devices = shared_devices.clone();
-        let view = shared_view.clone();
+        let mut state = shared_state.clone();
         move |(msg, addr): (Message, SocketAddr)| {
-            if let Some(ref gossip) = msg.payload {
-                match gossip {
-                    Gossip::DebugMessage { text } => info!("From {}  {}", addr, text),
-
-                    Gossip::DeviceGossip(device_gossip) => devices
-                        .lock()
-                        .expect("unable to lock devices database for update")
-                        .process(msg.sender, msg.timestamp_millis, device_gossip),
-
-                    Gossip::ViewGossip(view_gossip) => view
-                        .lock()
-                        .expect("unable to lock view for update")
-                        .process(msg.sender, msg.timestamp_millis, view_gossip),
-                }
-            }
+            msg.update_state(&mut state, &addr);
             Ok((msg, addr))
         }
     };
@@ -79,11 +73,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // view. Right now, it forwards the message to all servers in the
     // cluster, not just a subset.
     let gossip_future = {
-        let view = shared_view.clone();
+        let state = shared_state.clone();
         move |(mut msg, addr): (Message, SocketAddr)| {
             if msg.hops > 0 {
                 msg.hops -= 1;
-                let locked_view = view.lock().expect("unable to lock view for forwarding");
+                let locked_view = state
+                    .view
+                    .lock()
+                    .expect("unable to lock view for forwarding");
                 for (_uuid, info) in &locked_view.servers {
                     writer.start_send((msg.clone(), info.address.clone()))?;
                 }
